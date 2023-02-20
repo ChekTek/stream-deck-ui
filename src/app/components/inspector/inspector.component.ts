32a4d796b59c0b1311bedd6d43e54c2729b1a4dd
Zack Hoherchak <zhoherchak@gmail.com>
2023-02-20 15:51:40 -0500

    working websocket

--- src/app/components/inspector/inspector.component.ts
+++ src/app/components/inspector/inspector.component.ts
@@ -5,7 +5,8 @@ import { CommonModule } from '@angular/common';
 	selector: 'app-inspector',
 	standalone: true,
 	imports: [CommonModule],
-	template: ` <h2 class="text-xl font-extrabold text-gray-100 font">Property Inspector</h2>`,
+	template: ` <h2 class="text-xl font-extrabold text-gray-100 font">Property Inspector</h2>
+		<iframe src="assets/test.html"></iframe>`,
 	styleUrls: ['./inspector.component.css'],
 })
 export class InspectorComponent {}
